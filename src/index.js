e73ab12dd027cb1bde836490526279fbb0685c84
heerparmar <hiral.parmar@sparsh1.com>
2018-08-24 15:38:32 +0530

    - changes

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ program
     config.type
   ).option(
     '-d, --dir <pathToDirectory>',
-    'Path to the "components" directory (default: "src/components")',
+    'Path to the "components" directory (default: "src")',
     config.dir
   ).option(
     '-x, --extension <fileExtension>',
